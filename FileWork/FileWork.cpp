23fa84ead7004bdd3285f96f408c6e954e7c26d9
bifrurcated <55530887+bifrurcated@users.noreply.github.com>
2021-07-14 19:47:27 +1000

    Lesson #115-119 Work with file

--- FileWork/FileWork.cpp
+++ FileWork/FileWork.cpp
@@ -4,27 +4,16 @@
 
 using namespace std;
 
+/*
+	обработка исключений try catch
+*/
+
 int main()
 {
 	setlocale(LC_ALL, "RU");
-
-	string path = "myfile.txt";
-	ofstream fout;
-
-	fout.open(path, ofstream::app); //параметр для добавления в конец
-	if (!fout.is_open()) {
-		cout << "Ошибка открытия файла" << endl;
-		return -1;
-	}
-	cout << "Введите число!" << endl;
-	int a;
-	for (int i = 0; i < 5; i++)
-	{
-		cin >> a;
-		fout << a << "\n";
-	}
 	
-	fout.close();
+	string path = "operatorInOut.txt";
 
+	
 	return 0;
 }
